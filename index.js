2fb7b1fb415ceaef83d7014790ca032d4ad3eb0f
VinayKumar <239x1a33a6@gprec.ac.in>
2026-06-17 23:18:05 +0530

    Installe Cors for Communication

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const connectMongoDB = require("./connection");
+const cors = require("cors");
 const studentRouter = require("./routes/student.routes");
 const app = express();
 require("dotenv").config();
 app.use(express.urlencoded());
+app.use(cors());
 connectMongoDB(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
